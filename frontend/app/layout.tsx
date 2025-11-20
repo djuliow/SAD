3c49defb082fe8e9bd95c634d5e014fc1f4e31d2
Evangjelika <s22310385@student.unklab.ac.id>
2025-11-20 23:36:33 +0800

    feat(frontend): update layout, globals.css, button and package changes

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,20 +1,33 @@
 import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+
+import { cn } from "@/lib/utils";
 import { Toaster } from "sonner";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Sistem Informasi Klinik Sentosa",
-  description: "Workflow management for Klinik Sentosa"
+  description: "Workflow management for Klinik Sentosa",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bg-muted min-h-screen">
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         {children}
         <Toaster richColors position="top-right" />
       </body>
